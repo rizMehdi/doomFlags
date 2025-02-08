5c08c2fc7b70dbfa9e02a0daf7643fa5f9646558
rizMehdi <ccmeall@gmail.com>
2025-02-08 22:35:45 +0000

    add base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/doomflags/', // Ensure this matches your GitHub repository name
   plugins: [react()],
   test: {
     globals: true,
