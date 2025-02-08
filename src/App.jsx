518f8f605ecc4676d10c23077727f638a5940a67
rizMehdi <ccmeall@gmail.com>
2025-02-08 21:00:02 +0000

    change intro

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,11 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          Welcome to DoomFlags <span className="heart">♥️</span> 
         </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
+
         <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
         </p>
       </header>
     </div>
