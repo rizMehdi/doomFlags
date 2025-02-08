5e4dfe7676658385bc32f71b6a2a51372578134f
rizMehdi <ccmeall@gmail.com>
2025-02-08 21:48:26 +0000

    cap fixs

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   test: {
+//     globals: true,
+//     environment: 'jsdom',
+//   },
+// })
+
+
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-})
+  base: "/doomFlags/", // Ensure correct base path for GitHub Pages
+});
