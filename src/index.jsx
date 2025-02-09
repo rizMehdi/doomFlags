bcbd9a94207e447d47c2fab06f9b92a874eba957
rizMehdi <ccmeall@gmail.com>
2025-02-09 01:16:01 +0000

    fixdown2

--- src/index.jsx
+++ src/index.jsx
@@ -124,6 +124,8 @@ if (rootElement) {
 } else {
   console.error("Root element not found");
 }
+
+
 // import React, { useState } from "react";
 // import ReactDOM from "react-dom/client";
 // import "./index.css";
